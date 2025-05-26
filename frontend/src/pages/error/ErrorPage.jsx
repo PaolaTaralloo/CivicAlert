0ceee36b227c9cc098da107fdacd9b0b09025c3e
PaolaTarallo <paola.trll@gmail.com>
2025-05-26 15:05:35 +0200

    error page animazione con lottie

--- frontend/src/pages/error/ErrorPage.jsx
+++ frontend/src/pages/error/ErrorPage.jsx
@@ -1,12 +1,18 @@
 import { Container, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import Lottie from 'lottie-react';
+import animationData from '../../assets/notfound.json';
 
 const Error = () => {
   return (
     <Container className="text-center mt-5">
+      <div style={{ maxWidth: 400, margin: '0 auto' }}>
+        <Lottie animationData={animationData} loop={true} />
+      </div>
+
       <h1 className="display-4">404</h1>
       <p className="lead">Oops! Pagina non trovata.</p>
-      <p>La pagina che stai cercando non esiste o è stata rimossa.</p>
+      <p>La strada che stai cercando è bloccata... oppure non esiste!</p>
 
       <div className="d-flex justify-content-center gap-3 mt-4">
         <Button as={Link} to="/" variant="primary">
